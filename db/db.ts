a2aaf01f97be5ce599273c7f2e7a3702e92a1458
Andrew Huynh <andrew.huynh17@gmail.com>
2021-09-21 19:14:31 -0400

    db set implmented

--- db/db.ts
+++ db/db.ts
@@ -1,6 +1,11 @@
 // might want to use pool, since there might be a potential of many queries
 
 import { Pool } from "pg"
+console.log(process.env.DB_USER)
 
-const pool = new Pool()
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PW}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
+
+export const pool = new Pool(
+    { connectionString }
+)
